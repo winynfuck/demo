77e5a1b0b1f835fb9e406ffd44912338bbadec3d
winynfuck <38125063+winynfuck@users.noreply.github.com>
2018-12-02 15:39:38 +0800

    Update calcbackproject_objectFind_class.cpp

--- calcbackproject_objectFind_class.cpp
+++ calcbackproject_objectFind_class.cpp
@@ -1,38 +1,39 @@
 class ObjectFinder {
+	 //使用反投影直方图法检测特定的图像内容
 
 private:
 
-	float hranges[2];
-	const float* ranges[3];
+	float hranges[2];//最大最小值
+	const float* ranges[3];//每个通道的范围，由最大最小值求得
 	int channels[3];
 
-	float threshold;
-	cv::MatND histogram;
-	cv::SparseMat shistogram;
-	bool isSparse;
+	float threshold;//设置的阈值，用于最后结果的二值化
+	cv::MatND histogram;//感兴趣区域的直方图数组
+	cv::SparseMat shistogram;//稀疏矩阵
+	bool isSparse;//判断是否为稀疏矩阵
 
 public:
 
 	ObjectFinder() : threshold(0.1f), isSparse(false) {
 
-		ranges[0] = hranges; // all channels have the same range 
+		ranges[0] = hranges; // 所有通道设置为相同的最大最小值
 		ranges[1] = hranges;
 		ranges[2] = hranges;
 	}
 
-	// Sets the threshold on histogram values [0,1]
+	// 设置阈值为[0,1]
 	void setThreshold(float t) {
 
 		threshold = t;
 	}
 
-	// Gets the threshold
+	//获取阈值
 	float getThreshold() {
 
 		return threshold;
 	}
 
-	// Sets the reference histogram
+	//设置感兴趣区域的直方图并均一化
 	void setHistogram(const cv::MatND& h) {
 
 		isSparse = false;
@@ -40,7 +41,7 @@ public:
 		cv::normalize(histogram, histogram, 1.0);
 	}
 
-	// Sets the reference histogram
+	//稀疏矩阵的情况
 	void setHistogram(const cv::SparseMat& h) {
 
 		isSparse = true;
@@ -48,17 +49,17 @@ public:
 		cv::normalize(shistogram, shistogram, 1.0, cv::NORM_L2);
 	}
 
-	// Finds the pixels belonging to the histogram
+	// 识别的主要方法一
 	cv::Mat find(const cv::Mat& image) {
 
 		cv::Mat result;
 
 		hranges[0] = 0.0;	// range [0,255]
 		hranges[1] = 255.0;
-		channels[0] = 0;		// the three channels 
+		channels[0] = 0;		// 三通道
 		channels[1] = 1;
 		channels[2] = 2;
-
+		 //稀疏矩阵使用反投影直方图得到结果图像
 		if (isSparse) { // call the right function based on histogram type
 
 			cv::calcBackProject(&image,
@@ -72,7 +73,7 @@ public:
 
 		}
 		else {
-
+			//非稀疏矩阵使用反投影直方图得到结果图像
 			cv::calcBackProject(&image,
 				1,            // one image
 				channels,     // vector specifying what histogram dimensions belong to what image channels
@@ -84,12 +85,13 @@ public:
 		}
 
 
-		// Threshold back projection to obtain a binary image
+		// 二值化
 		if (threshold > 0.0)
 			cv::threshold(result, result, 255 * threshold, 255, cv::THRESH_BINARY);
 
 		return result;
 	}
+	 //主要识别方法二
 
 	cv::Mat find(const cv::Mat& image, float minValue, float maxValue, int *channels, int dim) {
 
